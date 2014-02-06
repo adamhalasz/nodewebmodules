4bdb5686438030fd35bb6bc841286fe62ccd1cf2
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2014-02-06 08:46:13 -0200

    added cron.start() function

--- cron.js
+++ cron.js
@@ -1,12 +1,13 @@
-module.exports = function() {
-	var 
-		CronJob = require('cron').CronJob,
-		Scraper = require('./scraper')
-	;
-		
+var 
+	CronJob = require('cron').CronJob,
+	Scraper = require('./scraper')
+;
+exports.start = function() {
+	
 	var job = new CronJob({
 		cronTime: '00 00 00 * * *',
 		onTick: function() {
+			console.log("Schedule modules update");
 			Scraper.start();
 		},
 		start: false
