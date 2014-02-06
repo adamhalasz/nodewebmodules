81ef673a02a00b161f5cc61bf60d411bb079867d
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2014-02-06 08:53:39 -0200

    Fix data keys

--- build_module.js
+++ build_module.js
@@ -6,20 +6,20 @@ var S = require('string')
 module.exports = function(gh, npm) {
 
 	var data = {};
-	data[name] = npm.name;
-	data[title] = S(npm.name).capitalize().s;
-	data[description] = gh.description;
-	data[version] = npm.version;
-	data[engine] = npm.engines && npm.engines.node;
-	data[created_at] = moment(gh.created_at).fromNow();
-	data[author] = npm.author && npm.author.name || npm._npmUser.name;
-	data[forks] = gh.forks_count;
-	data[watchers] = gh.watchers;
-	data[issues] = gh.open_issues;
-	data[install] = npm.name !== "meteor" ? ("npm install " + npm.name) : "curl https://install.meteor.com | /bin/sh";
-	data[gh_url] = gh.html_url;
-	data[npm_url] = npm.name !== "meteor" && ("https://npmjs.org/package/" + npm.name);
-	data[site] = gh.homepage && url.resolve('http://', gh.homepage);
+	data["name"] = npm.name;
+	data["title"] = S(npm.name).capitalize().s;
+	data["description"] = gh.description;
+	data["version"] = npm.version;
+	data["engine"] = npm.engines && npm.engines.node;
+	data["created_at"] = moment(gh.created_at).fromNow();
+	data["author"] = npm.author && npm.author.name || npm._npmUser.name;
+	data["forks"] = gh.forks_count;
+	data["watchers"] = gh.watchers;
+	data["issues"] = gh.open_issues;
+	data["install"] = npm.name !== "meteor" ? ("npm install " + npm.name) : "curl https://install.meteor.com | /bin/sh";
+	data["gh_url"] = gh.html_url;
+	data["npm_url"] = npm.name !== "meteor" && ("https://npmjs.org/package/" + npm.name);
+	data["site"] = gh.homepage && url.resolve('http://', gh.homepage);
 	
 	return data;
 };
